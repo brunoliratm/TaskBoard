b4b92cc0a3fccfdb0ee0abb42b96c1192428bf61
BrunoMagno <bmagnoserver@gmail.com>
2025-01-29 21:57:30 -0300

    Add header component and task service for task management functionality

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit() {
+  }
+
+  openDialog() {
+    // Logic to open a dialog
+  }
+
+  handleButtonClick() {
+    // Logic for button click
+  }
+}
