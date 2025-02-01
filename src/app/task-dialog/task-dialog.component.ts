61c16ea48f099351f8288f1d8f526d2ef4f7f48f
BrunoMagno <bmagnoserver@gmail.com>
2025-01-31 21:52:28 -0300

    Add task dialog component: implement form for adding tasks with title and description

---
+++ src/app/task-dialog/task-dialog.component.ts
@@ -0,0 +1,43 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Stage, Task } from '../services/stage.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-task-dialog',
+  templateUrl: './task-dialog.component.html',
+  styleUrls: ['./task-dialog.component.scss'],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule]
+})
+export class TaskDialogComponent {
+  taskForm: FormGroup;
+
+  constructor(
+    public dialogRef: MatDialogRef<TaskDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { stage: Stage },
+    private fb: FormBuilder
+  ) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['']
+    });
+  }
+
+  onSave(): void {
+    if (this.taskForm.valid) {
+      const task: Task = {
+        id: Math.random().toString(36).substring(2, 11),
+        title: this.taskForm.value.title,
+        description: this.taskForm.value.description
+      };
+      this.dialogRef.close(task);
+    }
+  }
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
+}
